dea788969edb38c7e3613cb681f189abbe095c7d
Uday Bhanu Singh <70534279+udaybhanusingh@users.noreply.github.com>
2026-06-02 19:21:57 +0000

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -351,4 +351,4 @@ for row in display_df.itertuples():
         else:
             display_df.at[row.Index, member] = ""
 
-st.table(display_df, height=300, border=False)
+st.table(display_df, height=300, border="Horizontal")
